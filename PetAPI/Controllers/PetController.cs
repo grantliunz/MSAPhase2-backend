94a234eff3f11d651f3c8fc2358ca8660d48e2ab
Grant Liu <grant.liux@gmail.com>
2022-07-31 14:45:53 +1200

    Initial Commit

---
+++ PetAPI/Controllers/PetController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PetAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PetController : ControllerBase
+    {
+        private static List<Pet> pets = new List<Pet>()
+        {
+            new Pet()
+            {
+                Id = 1,
+                Name = "Cleo",
+                Type = "Dog",
+                Age = 3
+            },
+            new Pet()
+            {
+                Id = 1,
+                Name = "Cleo",
+                Type = "Dog",
+                Age = 3
+            }
+        };
+
+
+
+    }
+}
---
+++ PetAPI/Pet.cs
@@ -0,0 +1,11 @@
+﻿namespace PetAPI
+{
+    public class Pet
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }  = String.Empty;
+        public string Name { get; set; } = String.Empty;
+        public int Age { get; set; }
+
+    }
+}
